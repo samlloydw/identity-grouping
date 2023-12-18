a22f35bb176b5c5ca074646fe90268e5a81f9209
samlloydw <samlloydw@gmail.com>
2023-12-18 17:58:01 +0000

    Wrote tests for grouping.

--- src/group.rs
+++ src/group.rs
@@ -33,20 +33,88 @@ pub fn atomic_group<T>(mut sequence: impl Iterator<Item=T>) -> Vec<(T, T)>
     groups
 }
 
+pub fn _ungroup<T>(_grouped: Vec<(T, T)>) -> Vec<T> {
+    todo!()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
-    fn setup_u32() -> impl Iterator<Item=u32> {
+    /// Module to test what happens if the atomic step size is varied.
+    mod evens {
+        use super::*;
+        use std::cmp::Ord;
+        use core::ops::Add;
+        
+        #[derive(PartialEq, Eq, Ord, PartialOrd, Clone, Copy, Debug)]
+        pub struct Evens(pub u32);
+
+        impl Add for Evens {
+            type Output = Self;
+            fn add(self, rhs: Self) -> Self::Output {
+                Self(self.0 + rhs.0)
+            }
+        }
+
+        impl AtomicStep for Evens {
+            fn atomic_step(&self) -> Self {
+                Self(2)
+            }
+        }
+
+    }
+
+    fn positives() -> impl Iterator<Item=i32> {
         (0..10).chain(20..31).chain(45..50).chain(60..61)
     }
 
-    #[test]
-    fn test_u32_atomic_group() {
-        let mut group = atomic_group(setup_u32());
-        assert_eq!(group, Vec::from([(0, 9), (20, 30), (45,49), (60, 60)]));
+    fn negatives() -> impl Iterator<Item=i32> {
+        (-50..-45).chain(-20..-5).chain(-3..10)
+    }
 
-        group = atomic_group((0..1));
+    fn overlap() -> impl Iterator<Item=i32> {
+        (-50..-45).chain(-48..-20).chain(0..10).chain(-5..10)
+    }
+
+    #[test]
+    fn test_empty() {
+        let mut group = atomic_group(0..1);
         assert_eq!(group, Vec::from([(0, 0)]));
+        group = atomic_group(0..0);
+        assert_eq!(group, Vec::new());
+    }
+
+    #[test]
+    fn test_positives() {
+        let group = atomic_group(positives());
+        assert_eq!(group, Vec::from([(0, 9), (20, 30), (45,49), (60, 60)]));
+        let chained_group = atomic_group(positives().chain(61..65));
+        assert_eq!(chained_group, Vec::from([(0, 9), (20, 30), (45,49), (60, 64)]));
+    }
+
+
+    #[test]
+    fn test_negatives() {
+        let group = atomic_group(negatives());
+        assert_eq!(group, Vec::from([(-50, -46), (-20, -6), (-3,9)]));
+    }
+
+    #[test]
+    fn test_overlap() {
+        let group = atomic_group(overlap());
+        assert_eq!(group, Vec::from([(-50, -46), (-48, -21), (0, 9), (-5, 9)]));
+    }
+
+    #[test]
+    fn larger_step() {
+        use evens::Evens;
+        let evens = Vec::from([Evens(2), Evens(4), Evens(6), Evens(10)]);
+        let group = atomic_group(evens.into_iter());
+        assert_eq!(group, Vec::from([( Evens(2), Evens(6) ), ( Evens(10), Evens(10) )]));
+        let evens = Vec::from([Evens(2), Evens(4), Evens(6), Evens(4), Evens(6), Evens(8)]);
+        let group = atomic_group(evens.into_iter());
+        assert_eq!(group, Vec::from([( Evens(2), Evens(6) ), ( Evens(4), Evens(8) )]));
+
     }
 }
